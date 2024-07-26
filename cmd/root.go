fbafde8040fe6537cbf09351456a0469bc1b5ee8
Alberto Cavalcante <alberto@cavalcante.uk>
2024-07-26 18:44:19 -0300

    prepare for unit tests: removed global vars in favor of option flags in structs and "New" constructor for Cobra Commands + addressed some linter complaints and added more descriptive messages to each flag

--- cmd/root.go
+++ cmd/root.go
@@ -14,60 +14,71 @@ const (
 	UnsuccessExitCode = 1
 )
 
-var (
-	source      string
-	destination string
-	fromFile    string
+type GarfFlags struct {
+	Source      string
+	Destination string
+	FromFile    string
+}
 
-	rootCmd = &cobra.Command{
+func (f *GarfFlags) addFlags(cmd *cobra.Command) {
+	flags := cmd.Flags()
+	flags.StringVarP(&f.Source, "source", "s", "", "GitHub Release URL to the artifact")
+	flags.StringVarP(&f.Destination, "destination", "d", "", "Artifacts destination (e.g. sandbox-generic-local)")
+	flags.StringVarP(&f.FromFile, "from-file", "f", "", "Skip Download. Upload from file and use URL to infer coordinates")
+}
+
+// NewRootCmd creates the root command for the CLI.
+func NewRootCmd() *cobra.Command {
+	f := &GarfFlags{}
+
+	cmd := &cobra.Command{
 		Use:   "garf",
 		Short: "garf is a CLI to mirror artifacts",
 		Long: `A CLI to mirror artifacts from places such as
 				GitHub Releases to registries such as 
 				JFrog Artifactory.`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if source == "" || destination == "" {
+			if f.Source == "" || f.Destination == "" {
 				return fmt.Errorf("--source and --destination flags are required")
 			}
 
-			artifactCoordinates, err := artifact.ExtractCoordinatesFromURL(source)
+			artifactCoordinates, err := artifact.ExtractCoordinatesFromURL(f.Source)
 			if err != nil {
 				return err
 			}
 			fmt.Printf("Artifact coordinates: %+v\n", artifactCoordinates)
 
-			fmt.Printf("Mirroring %s to %s\n", source, destination)
+			fmt.Printf("Mirroring %s to %s\n", f.Source, f.Destination)
 
 			var location string
-			if fromFile != "" {
-				location = fromFile
+			if f.FromFile != "" {
+				location = f.FromFile
 			} else {
 				defer os.RemoveAll(filepath.Dir(location))
 
-				location, err = core.DownloadArtifact(source)
+				location, err = core.DownloadArtifact(f.Source)
 				if err != nil {
 					return err
 				}
 				fmt.Printf("Artifact downloaded to %s\n", location)
 			}
 
-			err = core.UploadGenericArtifact(location, destination, artifactCoordinates)
+			err = core.UploadGenericArtifact(location, f.Destination, artifactCoordinates)
 			if err != nil {
 				return err
 			}
+
 			return nil
 		},
 	}
-)
 
-func init() {
-	rootCmd.Flags().StringVarP(&source, "source", "s", "", "Source of the artifacts")
-	rootCmd.Flags().StringVarP(&destination, "destination", "d", "", "Destination of the artifacts")
-	rootCmd.Flags().StringVarP(&fromFile, "from-file", "f", "", "Use the source URL only for coordinates and upload the file at the specified path")
+	f.addFlags(cmd)
+
+	return cmd
 }
 
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
+	if err := NewRootCmd().Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(UnsuccessExitCode)
 	}
